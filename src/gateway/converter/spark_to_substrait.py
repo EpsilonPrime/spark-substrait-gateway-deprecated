23a6711ac542fb3a469383ef916e57a510395572
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-03-01 23:57:37 -0800

    Move precursor to symbol table into its own file.

--- src/gateway/converter/spark_to_substrait.py
+++ src/gateway/converter/spark_to_substrait.py
@@ -1,9 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 """Routines to convert SparkConnect plans to Substrait plans."""
 from collections import defaultdict
-import dataclasses
 import operator
-from typing import Dict, List
+from typing import Dict
 
 from substrait.gen.proto import plan_pb2
 from substrait.gen.proto import algebra_pb2
@@ -14,14 +13,7 @@ import spark.connect.expressions_pb2 as spark_exprs_pb2
 import spark.connect.relations_pb2 as spark_relations_pb2
 
 from gateway.converter.spark_functions import ExtensionFunction, lookup_spark_function
-
-
-@dataclasses.dataclass
-class PlanMetadata:
-    """Tracks various information about a specific plan id."""
-    parent_plan_id: int
-    input_fields: List[str]  # And maybe type
-    output_fields: List[str]
+from gateway.converter.symbol_table import PlanMetadata
 
 
 # pylint: disable=E1101,fixme,too-many-public-methods
