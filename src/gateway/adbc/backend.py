4033db34420bff97101421fcf97c757e02285a57
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-03-04 17:59:28 -0800

    Add code that should have worked but there's a duckdb unescaping issue in the C++ library.

--- src/gateway/adbc/backend.py
+++ src/gateway/adbc/backend.py
@@ -5,15 +5,17 @@ import adbc_driver_duckdb.dbapi
 from substrait.gen.proto import plan_pb2
 
 
+# pylint: disable=too-few-public-methods
 class AdbcBackend:
     """Provides methods for contacting an ADBC backend via Substrait."""
 
     def __init__(self):
         pass
 
-    def execute(self, plan: plan_pb2.Plan) -> str:
+    def execute(self, plan: 'plan_pb2.Plan') -> str:
         """Executes the given Substrait plan against the associated ADBC backend."""
-        with adbc_driver_duckdb.dbapi.connect("test.db") as conn, conn.cursor() as cur:
-            cur.adbc_statement.set_substrait_plan(plan.SerializeToString())
+        with adbc_driver_duckdb.dbapi.connect() as conn, conn.cursor() as cur:
+            plan_data = plan.SerializeToString()
+            cur.adbc_statement.set_substrait_plan(plan_data)
             tbl = cur.fetch_arrow_table()
             return f'{tbl}'
