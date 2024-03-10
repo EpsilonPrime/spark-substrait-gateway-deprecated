546feb22360a68ee328a3b6b1dc2272062274aa6
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-03-10 03:48:00 -0700

    Now with a usable standalone client demo.

---
+++ src/gateway/demo/client_demo.py
@@ -0,0 +1,44 @@
+# SPDX-License-Identifier: Apache-2.0
+'''A PySpark client that can send sample queries to the gateway.'''
+import atexit
+from pathlib import Path
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, desc
+from pyspark.sql.pandas.types import from_arrow_schema
+
+from gateway.demo.mystream_database import create_mystream_database, delete_mystream_database
+from gateway.demo.mystream_database import get_mystream_schema
+
+# The gateway currently does not support determining the schema from a dataset.
+USE_SCHEMA_DETECTION = False
+
+
+def execute_query() -> None:
+    """Runs a single sample query against the gateway."""
+    spark = SparkSession.builder.remote('sc://localhost:50051').getOrCreate()
+
+    users_dir = str(Path('users.parquet').absolute())
+
+    if USE_SCHEMA_DETECTION:
+        schema_users = spark.read.parquet(users_dir).schema
+    else:
+        schema_users = get_mystream_schema('users')
+
+    df_users = spark.read.format('parquet') \
+        .schema(from_arrow_schema(schema_users)) \
+        .parquet(users_dir)
+
+    # pylint: disable=singleton-comparison
+    df_users2 = df_users \
+        .filter(col('paid_for_service') == True) \
+        .sort(desc('name')) \
+        .limit(10)
+
+    df_users2.show()
+
+
+if __name__ == '__main__':
+    atexit.register(delete_mystream_database)
+    path = create_mystream_database()
+    execute_query()
