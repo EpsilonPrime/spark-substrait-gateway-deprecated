853a6a4a8be0d9e7e18d4eeb4d1f8bbc1fccdd6d
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-04-17 18:53:55 -0700

    Added some rounding.

--- src/gateway/tests/test_tpch_with_dataframe_api.py
+++ src/gateway/tests/test_tpch_with_dataframe_api.py
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-"""Tests for the Spark to Substrait Gateway server."""
-from hamcrest import assert_that, equal_to
+"""TPC-H Dataframe tests for the Spark to Substrait Gateway server."""
 from pyspark import Row
 from pyspark.sql.functions import col, substring, avg, count, try_sum
 from pyspark.testing import assertDataFrameEqual
@@ -14,10 +13,10 @@ class TestTpchWithDataFrameAPI:
     # pylint: disable=singleton-comparison
     def test_query_01(self, spark_session_with_tpch_dataset):
         expected = [
-            Row(l_returnflag='A', l_linestatus='F', sum_qty=37734107.0,
-                   sum_base_price=56586554400.730194, sum_disc_price=None, sum_charge=None,
-                   avg_qty=25.522005853257337, avg_price=38273.129734621805,
-                   avg_disc=0.04998529583842897, count_order=1478493),
+            Row(l_returnflag='A', l_linestatus='F', sum_qty=37734107.00,
+                sum_base_price=56586554400.73, sum_disc_price=53758257134.87,
+                sum_charge=55909065222.83, avg_qty=25.52,
+                avg_price=38273.13, avg_disc=0.05, count_order=1478493),
         ]
 
         lineitem = spark_session_with_tpch_dataset.table('lineitem')
@@ -25,13 +24,13 @@ class TestTpchWithDataFrameAPI:
                                                                              'l_linestatus').agg(
             try_sum('l_quantity').alias('sum_qty'),
             try_sum('l_extendedprice').alias('sum_base_price'),
-            try_sum('l_extendedprice' * (1 - col('l_discount'))).alias('sum_disc_price'),
-            try_sum('l_extendedprice' * (1 - col('l_discount')) * (1 + col('l_tax'))).alias('sum_charge'),
+            try_sum(col('l_extendedprice') * (1 - col('l_discount'))).alias('sum_disc_price'),
+            try_sum(col('l_extendedprice') * (1 - col('l_discount')) * (1 + col('l_tax'))).alias(
+                'sum_charge'),
             avg('l_quantity').alias('avg_qty'),
             avg('l_extendedprice').alias('avg_price'),
             avg('l_discount').alias('avg_disc'),
             count('*').alias('count_order'))
 
         sorted_outcome = outcome.sort('l_returnflag', 'l_linestatus').limit(1).collect()
-        assertDataFrameEqual(sorted_outcome, expected)
-
+        assertDataFrameEqual(sorted_outcome, expected, rtol=1e-2)
