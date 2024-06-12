90fed4ef6baa8e43cf75bd16db96142bbb2d6d17
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-06-11 18:14:55 -0700

    Add moved notice.

--- conftest.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-"""Configuration for pytest."""
-
-
-def pytest_addoption(parser):
-    """Adds the rebuild goldens command line option to pytest."""
-    parser.addoption("--rebuild_goldens", action="store", default=False)
--- src/gateway/converter/data/select.sql
+++
@@ -1,1 +0,0 @@
-SELECT c_custkey, c_phone, c_mktsegment FROM customer
